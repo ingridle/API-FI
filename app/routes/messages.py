0a1c3619d7e1e67228716426e468e9bac9ecc3c3
ingreed <108835405+ingridle@users.noreply.github.com>
2025-07-31 18:23:44 +0000

    terceiro

--- app/routes/messages.py
+++ app/routes/messages.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, request
-from ..models.message import Message
 from .. import db
 from ..schemas.message_schema import MessageSchema
+from ..controllers import message_controller
+from ..middlewares.message_required import mensagem_existe
 
 messages_bp = Blueprint('messages', __name__)
 message_schema = MessageSchema()
@@ -9,48 +10,37 @@ messages_schema = MessageSchema(many=True)
 
 @messages_bp.route('/', methods=['GET'])
 def get_messages():
-    messages = Message.query.all()
+    messages = message_controller.listar_mensagens()
     return messages_schema.jsonify(messages), 200
 
 @messages_bp.route('/<int:message_id>', methods=['GET'])
+@mensagem_existe
 def get_message(message_id):
-    message = Message.query.get_or_404(message_id)
-    return message_schema.jsonify(message), 200
+    return message_schema.jsonify(request.mensagem), 200
 
 @messages_bp.route('/', methods=['POST'])
 def create_message():
     data = message_schema.load(request.get_json())
-    message = Message(**data)
-    db.session.add(message)
-    db.session.commit()
+    message = message_controller.criar_mensagem(data)
     return message_schema.jsonify(message), 201
 
 @messages_bp.route('/<int:message_id>', methods=['PUT'])
+@mensagem_existe
 def update_message(message_id):
-    message = Message.query.get_or_404(message_id)
-    data = message_schema.load(request.get_json())
+    data = message_schema.load(request.get_json())  # Atualização completa
+    updated = message_controller.atualizar_mensagem(request.mensagem, data)
+    return message_schema.jsonify(updated), 200
 
-    for key, value in data.items():
-        setattr(message, key, value)
-
-    db.session.commit()
-    return message_schema.jsonify(message), 200
-    
 @messages_bp.route('/<int:message_id>', methods=['PATCH'])
+@mensagem_existe
 def partial_update_message(message_id):
-    message = Message.query.get_or_404(message_id)
-    data = message_schema.load(request.get_json(), partial=True)
-
-    for key, value in data.items():
-        setattr(message, key, value)
-
-    db.session.commit()
-    return message_schema.jsonify(message), 200
+    data = message_schema.load(request.get_json(), partial=True)  # Atualização parcial
+    updated = message_controller.atualizar_mensagem(request.mensagem, data)
+    return message_schema.jsonify(updated), 200
 
 @messages_bp.route('/<int:message_id>', methods=['DELETE'])
+@mensagem_existe
 def delete_message(message_id):
-    message = Message.query.get_or_404(message_id)
-    db.session.delete(message)
-    db.session.commit()
+    message_controller.deletar_mensagem(request.mensagem)
     return '', 204
 
